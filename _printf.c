19f501d1d37ead87bbccc1a28726e2722ed8b5c2
William96 <williamvulandi@icloud.com>
2023-09-18 14:12:56 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -3,19 +3,6 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
-
-/**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
@@ -42,7 +29,7 @@ int _printf(const char *format, ...)
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
-else
+		else
 		{
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
@@ -51,7 +38,7 @@ else
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-						flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
